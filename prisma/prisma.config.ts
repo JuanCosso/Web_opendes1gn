abd322501081d49aba99201d566cacf38f3fbbc4
JuanCosso <juancossoyt@gmail.com>
2026-03-14 21:25:56 -0300

    beta 1.6

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -2,7 +2,6 @@ import path from "path";
 import type { PrismaConfig } from "prisma";
 
 export default {
-  earlyAccess: true,
   schema: path.join("prisma", "schema.prisma"),
   migrate: {
     async adapter() {
